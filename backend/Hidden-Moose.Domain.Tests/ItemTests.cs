f32e526db63f8829b26f63185acc4a00dd77b13f
Jordan Klapper <djk0117@gmail.com>
2021-04-28 11:37:55 -0400

    Testing changes (Coverage Numbers off from paper)

---
+++ backend/Hidden-Moose.Domain.Tests/ItemTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Hidden.Moose.Domain.Catalog;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hidden_Moose.Domain.Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Item()
+        {
+            var item = new Item("Name","Description", "Brand",10.00m);
+
+            Assert.AreEqual("Name",item.Name);
+            Assert.AreEqual("Description", item.Description);
+            Assert.AreEqual("Brand", item.Brand);
+            Assert.AreEqual(10.00m, item.Price);
+        }
+    }
+}
